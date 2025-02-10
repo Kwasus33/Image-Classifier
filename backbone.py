8f7eb63a08bb18d614955d8485595cc8aa8532c2
Jakub Mieczkowski <jmieczkowski04@gmail.com>
2025-02-10 14:41:14 +0100

    Added basic backbone

---
+++ backbone.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+
+
+class GolemBackbone(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(num_features=32)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout = nn.Dropout(0.5)
+        self.fc = nn.Linear(32 * 14 * 14, 1000)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.pool(x)
+        x = self.dropout(x)
+        return self.fc(x)
