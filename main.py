c0cb480c1fe830c224612482d4c5b6627ff8f256
Jakub Kwaśniak <kubak04@outlook.com>
2025-02-12 23:28:58 +0100

    added generating nn by running script with parser argumets

--- main.py
+++ main.py
@@ -2,21 +2,65 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.models as models
+import argparse
 
+from backbone import GolemBackbone
 from classifier import GolemClassifier
 from dataloaders import get_dataloaders
 from model_utils import train_epoch, eval
 
+from model_factories import (
+    get_golem_model,
+    get_resnet18_model,
+    get_resnet34_model,
+    get_vitbase_model,
+)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--optim", type=str, required=True)
+    parser.add_argument("--loss", type=str, required=True)
+    parser.add_argument("--backbone", type=str, required=True)
+    return parser.parse_args()
 
 
 def main():
     train_loader, test_loader, classes = get_dataloaders()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    resnet18 = models.resnet18()
-    gc = GolemClassifier(resnet18, resnet18.fc.out_features)
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(gc.parameters(), lr=1e-3)
+    args = parse_arguments()
+
+    if args.BACKBONE == "resnet18":
+        gc = get_resnet18_model()
+    elif args.BACKBONE == "resnet34":
+        gc = get_resnet34_model()
+    elif args.BACKBONE == "ViT":
+        gc = get_vitbase_model()
+    else:
+        gc = get_golem_model()
+
+    criterion = (
+        nn.CrossEntropyLoss() if args.LOSS == "CrossEntropy" else nn.MSELoss
+    )  # if args.LOSS == "CrossEntropy" else RMSE
+
+    optimizer = (
+        optim.SGD(gc.parameters(), lr=1e-3)
+        if args.OPTIM == "sgd"
+        else (
+            optim.RMSprop(gc.parameters(), lr=1e-3)
+            if args.OPTIM == "rms"
+            else (
+                optim.Adadelta(gc.parameters(), lr=1e-3)
+                if args.OPTIM == "adadelta"
+                else (
+                    optim.Adamax(gc.parameters(), lr=1e-3)
+                    if args.OPTIM == "adamax"
+                    else optim.Adam(gc.parameters(), lr=1e-3)
+                )
+            )
+        )
+    )
 
     for i in range(2):
         loss = train_epoch(gc, criterion, optimizer, train_loader, device)
