3ea6a82ab30ae5e3abcddffbac6d825b7925ee99
Jakub Kwaśniak <kubak04@outlook.com>
2025-02-18 14:46:48 +0100

    added func for finding best model hyperparams

--- model_utils.py
+++ model_utils.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 import torch
+import torch.optim as optim
 import numpy as np
+import itertools
 from sklearn.metrics import recall_score, precision_score, f1_score
 
 
@@ -74,3 +76,56 @@ def eval(
     metrics = (loss, accuracy, precision, recall, f1)
 
     return metrics, preds
+
+
+def getBestOptimLoss():
+    # Loss functions
+    criterions = [
+        nn.CrossEntropyLoss,
+        # CrossEntropy is meant to be used in multiclass classification, automatically deals with different size tensors
+        # nn.MSELoss --> to use MSELoss we need to set same size of outputs (num_of_classes = 10 or 100) and labels (batch_size = 32)
+    ]
+
+    # Optimizers
+    optimizers = [
+        optim.Adam,
+        optim.SGD,
+        optim.RMSprop,
+        #   optim.Adadelta, optim.Adamax
+    ]
+
+    combinations = list(itertools.product(criterions, optimizers))
+
+    return combinations
+
+
+def getBestModelParams(model, loader, device):
+    min_loss = np.inf
+    best_lr = 0
+    best_optim = None
+    best_lossFunc = None
+
+    combinations = getBestOptimLoss()
+    # Finds optimal learning rate for most popular CNN funcs:
+    for lossFunc, optim in combinations:
+        print(f"Test for optim {optim} and loss func {lossFunc}: ")
+        for lr in np.array([0.0001, 0.001, 0.01, 0.1]):
+            print(f"Learning rate: {lr}")
+            criterion = lossFunc()
+            optimizer = optim(params=model.parameters(), lr=lr)
+            losses = []
+
+            for i in range(3):
+                loss = train_epoch(model, criterion, optimizer, loader, device)
+                print(f"{i+1}/3: loss={loss}")
+                if (i != 0 and loss > losses[-1]) or np.isnan(loss):
+                    break
+                losses.append(loss)
+
+            if len(losses) != 0 and losses[-1] < min_loss:
+                best_lr = lr
+                min_loss = losses[-1]
+                best_lossFunc = lossFunc
+                best_optim = optim
+
+    return best_lr, min_loss, best_lossFunc, best_optim
