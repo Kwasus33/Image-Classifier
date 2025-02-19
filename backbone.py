cf14023ace523a5fba743977606fcb8152ec63ac
Jakub Mieczkowski <jmieczkowski04@gmail.com>
2025-02-19 11:17:21 +0100

    Better backbone

--- backbone.py
+++ backbone.py
@@ -8,21 +8,65 @@ needs adding proper activation funcs
 class GolemBackbone(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(
-            in_channels=3, out_channels=32, kernel_size=3, padding=1
-        )  # in_channels=3 cause data(images) is represeted by 3 values (RGB)
-        # batch size is set to 32
-        self.bn1 = nn.BatchNorm2d(num_features=32)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.dropout = nn.Dropout(0.5)
-        self.fc = nn.Linear(32 * 16 * 16, 1000)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.relu3 = nn.ReLU()
+
+        self.conv4 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.relu4 = nn.ReLU()
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv5 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm2d(512)
+        self.relu5 = nn.ReLU()
+
+        self.conv6 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.bn6 = nn.BatchNorm2d(512)
+        self.relu6 = nn.ReLU()
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv7 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.bn7 = nn.BatchNorm2d(512)
+        self.relu7 = nn.ReLU()
+
+        self.conv8 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.bn8 = nn.BatchNorm2d(512)
+        self.relu8 = nn.ReLU()
+        self.pool5 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.fc1 = nn.Linear(512, 1024)
+        self.relu9 = nn.ReLU()
+        self.dropout1 = nn.Dropout(0.5)
+        self.fc = nn.Linear(1024, 1000)
+        self.dropout2 = nn.Dropout(0.5)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.pool(x)
-        x = self.dropout(x)
+        x = self.pool1(self.relu1(self.bn1(self.conv1(x))))
+        x = self.pool2(self.relu2(self.bn2(self.conv2(x))))
+        x = self.relu3(self.bn3(self.conv3(x)))
+        x = self.pool3(self.relu4(self.bn4(self.conv4(x))))
+        x = self.relu5(self.bn5(self.conv5(x)))
+        x = self.pool4(self.relu6(self.bn6(self.conv6(x))))
+        x = self.relu7(self.bn7(self.conv7(x)))
+        x = self.pool5(self.relu8(self.bn8(self.conv8(x))))
+
+        x = self.avgpool(x)
         x = x.view(
             x.size(0), -1
         )  # tensor must be flatten before passing to fully connected layer
-        return self.fc(x)
+        x = self.dropout1(self.relu9(self.fc1(x)))
+        x = self.dropout2(self.fc(x))
+        return x
